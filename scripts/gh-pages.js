6c2bd8e1b6f16aa158fe1b444a1bb75cb184809d
mo <feng_zilong@163.com>
2016-08-29 02:18:58 +0800

    Added gh-pages

---
+++ scripts/gh-pages.js
@@ -0,0 +1,11 @@
+var ghpages = require( 'gh-pages' );
+var path = require( 'path' );
+var cwd = process.cwd();
+
+ghpages.clean();
+ghpages.publish( path.resolve( cwd, 'public' ), {
+	logger: function(message) {
+		console.log(message);
+	}
+}, function() {
+} );
