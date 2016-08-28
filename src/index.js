dadbae606a18148b6ce1709ee0cee95ac32ec389
mo <feng_zilong@163.com>
2016-08-29 00:01:24 +0800

    Clean code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import View from './view';
 
 export default class Bee extends Observer {
 	constructor( { data, template } ) {
-		console.log( data );
 		super( data );
 		this.view = new View({
 			template: template,
